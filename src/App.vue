ec18a06dd27de697593bb9cd9654426498dc992b
jambackie <jambackie@gmail.com>
2022-07-08 19:16:16 +0300

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <template>
-  <div id="app">app</div>
+  <div id="app">
+    <HeaderComponent />
+  </div>
 </template>
 
 <script>
+import HeaderComponent from "./components/HeaderComponent.vue";
+
 export default {
   name: "App",
+  components: { HeaderComponent },
 };
 </script>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="header">
+    <div class="container header__container">
+      <a href="#">
+        <img class="header__logo" src="../assets/svg/sprite.svg#logo" alt="" />
+      </a>
+      <ul class="menu">
+        <li class="menu__item">
+          <a href="#" class="menu__link">Home</a>
+        </li>
+        <li class="menu__item">
+          <a href="#" class="menu__link">Catalog</a>
+        </li>
+        <li class="menu__item">
+          <a href="#" class="menu__link">Tenders</a>
+        </li>
+        <li class="menu__item">
+          <a href="#" class="menu__link">Trade</a>
+        </li>
+        <li class="menu__item">
+          <a href="#" class="menu__link">Web-Shop NL</a>
+        </li>
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderComponent",
+};
+</script>
